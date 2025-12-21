0bf535290e0a826d8f6552deaaecbe61d0235de7
varshil13 <shahvarshil13022000@gmail.com>
2025-12-21 18:10:11 +0530

    Define Basic Structure Of SpringMVC

---
+++ src/main/java/com/springboot/springMvc/dto/EmployeeDTO.java
@@ -0,0 +1,15 @@
+package com.springboot.springMvc.dto;
+
+import java.time.LocalDate;
+
+public class EmployeeDTO
+{
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private String role;
+    private Double salary;
+    private LocalDate dateOfJoining;
+
+}
